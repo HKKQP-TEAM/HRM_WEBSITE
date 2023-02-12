44636bfb5101f8d522200adba3d10d0d50e9a853
hamongkhang <hamongkhang@gmail.com>
2023-02-13 04:11:36 +0700

    set up react router

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
     ],
-    "overrides": [
+    'overrides': [
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "prettier"
+    'plugins': [
+        'react',
+        '@typescript-eslint',
+        'prettier',
+        'no-smart-quotes',
     ],
-    "rules": {
-        "rules": {
-            "@nab/nab-x/no-restricted-modules-in-mini-apps": "error",
-            "@nab/nab-x/no-apploader": "error",
-            "@typescript-eslint/no-unused-vars": "error",
-            "no-smart-quotes/no-smart-quotes": "error",
-            "@typescript-eslint/no-empty-function": "off",
+    'rules': {
+            '@typescript-eslint/no-unused-vars': 'error',
+            'react/react-in-jsx-scope': 'off',
+            'camelcase': 'error',
+            'spaced-comment': 'error',
+            'no-duplicate-imports': 'error',
+            'no-unused-vars': 'off',
+            'no-smart-quotes/no-smart-quotes': 'error',
+            '@typescript-eslint/no-empty-function': 'off',
             // turn on errors for missing imports
-            "import/no-unresolved": "error",
-            "import/default": "off",
-            "prettier/prettier": 2,
-            "import/order": [
-                "error",
-                {
-                    "groups": [
-                        "builtin",
-                        "external",
-                        "internal",
-                        ["parent", "sibling"], // Combine parent and sibling together
-                        "index"
-                    ],
-                    "newlines-between": "always"
-                }
-            ]
-        },
+            'import/default': 'off',
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import * as React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
